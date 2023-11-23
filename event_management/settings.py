caae98a603cacee9f62d24bdd16c34bdb3ba1f8d
Sadhvi Dupper <143220635+SadhviDupper@users.noreply.github.com>
2023-11-24 01:34:48 +0530

    setting
    setting

--- event_management/settings.py
+++ event_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gvv(&d^k0f5^xgqa+#ct4sxcg5%&5q&k2d(!uek5m+qj#b^0#2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webeventportal.azurewebsites.net', 'localhost']
 
 
 # Application definition
